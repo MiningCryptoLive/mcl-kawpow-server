3eb2d1ea35c212629b974ac8d5897be68de5499c
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2022-10-05 18:24:42 +0200

    Update stats.js

--- libs/stats.js
+++ libs/stats.js
@@ -57,24 +57,24 @@ module.exports = function (portalConfig, poolConfigs) {
             logger.error('Stats Redis Encountered An Error! Message: %s', JSON.stringify(err));
         });
     }
-    function gatherStatHistory() {
-        var retentionTime = (((Date.now() / 1000) - portalConfig.website.stats.historicalRetention) | 0).toString();
-        redisStats.ZRANGEBYSCORE(['statHistory', retentionTime, '+inf'], function (err, replies) {
-            if (err) {
-                logger.error('Error When Trying To Grab Historical Stat Data! Message: %s', JSON.stringify(err));
-                return;
-            }
-            for (var i = 0; i < replies.length; i++) {
-                _this.statHistory.push(JSON.parse(replies[i]));
-            }
-            _this.statHistory = _this.statHistory.sort(function (a, b) {
-                return a.time - b.time;
-            });
-            _this.statHistory.forEach(function (stats) {
-                addStatPoolHistory(stats);
-            });
-        });
-    }
+	function gatherStatHistory() {
+		var retentionTime = (((Date.now() / 1000) - portalConfig.website.stats.historicalRetention) | 0).toString();
+		redisStats.zrangebyscore(['statHistory', retentionTime, '+inf'], function(err, replies) {
+			if (err) {
+				logger.error('Error When Trying To Grab Historical Stat Data! Message: %s', JSON.stringify(err));
+				return;
+			}
+			for (var i = 0; i < replies.length; i++) {
+				_this.statHistory.push(JSON.parse(replies[i]));
+			}
+			_this.statHistory = _this.statHistory.sort(function(a, b) {
+				return a.time - b.time;
+			});
+      			_this.statHistory.forEach(function(stats) {
+				addStatPoolHistory(stats);
+			});
+		});
+	}
     function addStatPoolHistory(stats) {
         var data = {
             time: stats.time,
